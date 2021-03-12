4066b25552281aa63f7603f6021d30159dcdda97
Zahra <zbehfarshad@gmail.com>
2021-03-12 12:38:41 -0800

    Updating the immigration component

---
+++ src/components/Immigration/Immigration.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import details from '../../assets/Images/details.png';
+
+function Immigration() {
+    return (
+        <div>
+            <h1>General Information about Immigration Law</h1>
+            <img src={details} alt="List of info"/>
+        </div>
+    )
+}
+
+export default Immigration
