5c2e9d4e12cdcee93b04eaea344e4a14481371f3
Zahra <zbehfarshad@gmail.com>
2021-03-12 14:24:59 -0800

    Adding the toggle

--- src/components/ImmigrationService/ImmigrationService.jsx
+++ src/components/ImmigrationService/ImmigrationService.jsx
@@ -4,26 +4,50 @@ import testimonial from '../../assets/Images/testimonial.png';
 import detailsP1 from '../../assets/Images/details-p1.png';
 import video from '../../assets/Images/video.png';
 import questions from '../../assets/Images/questions.png';
+import accessability from '../../assets/Images/accessability.png';
+import access2 from '../../assets/Images/access2.png';
 import './ImmigrationService.scss';
 
-function ImmigrationService() {
-    return (
-        <div className="immigration-details">
-            <h1 className="immigration-details__title">General Information about Immigration Law</h1>
-            <img src={detailsList} alt="List of info" className="immigration-details__image"/>
-            <img src={testimonial} alt="Testimonial"  className="immigration-details__image img2"/>
-            <h2>Why hire an immigration lawyer?</h2>
-            <div className="immigration-details__row">
-                <div className="immigration-details__column-left">
-                    <img src={detailsP1} alt="Details "/>
-                </div>
-                <div className="immigration-details__column-right">
-                    <img src={video} alt="Details video"/>
-                    <img src={questions} alt="Details "/>
+class ImmigrationService extends React.Component{
+    state = {
+        access: false
+    };
+
+    handleClick = (event ) => {
+        event.preventDefault();
+        // this.setState({access: !this.state.access});
+        this.setState({access: true});
+    }
+
+    render () {
+        // const accessFirstclass = this.state.access ? "display--Now" : "display";
+        let first = "display";
+        let second = "display-not"
+        if(this.state.access) {
+            first = "display-not";
+            second = "display";
+        }
+
+        return (
+            <div className="immigration-details">
+                <h1 className="immigration-details__title">General Information about Immigration Law</h1>
+                <img src={detailsList} alt="List of info" className="immigration-details__image"/>
+                <img src={testimonial} alt="Testimonial"  className="immigration-details__image img2"/>
+                <h2>Why hire an immigration lawyer?</h2>
+                <div className="immigration-details__row">
+                    <div className="immigration-details__column-left">
+                        <img src={detailsP1} alt="Details "/>
+                    </div>
+                    <div className="immigration-details__column-right">
+                        <img src={video} alt="Details video"/>
+                        <img src={questions} alt="Details "/>
+                        <img src={accessability} alt="Accessability" className={first} onClick={this.handleClick}/>
+                        <img src={access2} alt="Accessability" className={second}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default ImmigrationService;
