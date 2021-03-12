bc5b75a90f5f019822d300e0491a29eaf3feea7f
James Lee <jameslee0415@hotmail.com>
2021-03-12 10:51:16 -0800

    added Navbar component

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Navbar.scss';
+import Logo from '../../../assets/Logo/Clio-Logo.svg';
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <h1>hello</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <nav className="nav">
+        <img src={Logo} alt="" className="nav__logo" />
+        <ul className="nav__list">
+          <li className="nav__item">Clio Business</li>
+          <li className="nav__item">Features</li>
+          <li className="nav__item">Lawyer Network</li>
+          <li className="nav__item">Resources</li>
+        </ul>
+        <div className="nav__button-div">
+          <button className="nav__loginButton">Login</button>
+          <button className="nav__signUpButton">Sign Up</button>
+        </div>
+      </nav>
+    );
+  }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import Header from '../components/Header/Header';
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <h1>Home</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default Home;
