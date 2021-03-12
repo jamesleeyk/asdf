0dd3b271cf1864ed26408ccf9cd16e3e56ab140c
Zahra <zbehfarshad@gmail.com>
2021-03-12 09:43:55 -0800

    Removing logo and fixing the typos and importing the Header in App.js

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Navbar from './Navbar/Navbar';
 
 class Header extends Component {
     render() {
         return (
             <div>
-                
+                <Navbar />
             </div>
         )
     }
