ec5bac6aafb8096747d82cc1d2591d80597aacc2
Zahra <zbehfarshad@gmail.com>
2021-03-12 11:43:44 -0800

    Adding the law-types pages

---
+++ src/pages/LawTypes.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LawTypes extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Law Types</h1>
+            </div>
+        )
+    }
+}
